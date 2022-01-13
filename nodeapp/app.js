3c76cf06063e9142d146a527322d525a9223057a
Rodolfo Rodriguez <rodolfo2smr@gmail.com>
2022-01-13 23:26:54 +0100

    feat: ejs, varios tipos de params, validadores, etc...

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -13,16 +13,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Añado una variable global, desde la que 
+//tengan acceso todas las vistas de la aplicación
+app.locals.title = 'NodeApp';
+
 /**
  * Middlewares de nuestra aplicación
  * Los evalua Expres ante cada petición que reciba
  */
-app.use(logger('dev'));
-app.use(express.json());
+app.use(logger('dev')); // middleware de log
+app.use(express.json()); //
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//mdw de ficheros estáticos
+app.use(express.static(path.join(__dirname, 'public'))); //esto evalúa los ficheros estáticos que cuelgan de /public por ejemplo --> /stylesheets/style.css 
 
 /**
  * Rutas de mi website
@@ -40,6 +45,16 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+
+    //gestionando error de validación
+    if (err.array) {
+        //error de validación
+        err.status = 422;
+        const errInfo = err.array({ onlyFirstError: true })[0];
+        console.log(errInfo);
+        err.message = `(${errInfo.location}) ${errInfo.param} - ${errInfo.msg}`;
+    }
+
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -1,13 +1,72 @@
 var express = require('express');
 var router = express.Router();
+const { query, body, param, validationResult } = require('express-validator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    const segundo = new Date().getSeconds();
+    //una forma de pasar variables a la vista
+    //metiendola en res.locals
+    //esto solo serviría en la vista index
+    res.locals.ejemplo = '<script>alert("Esto es un ejemplo")</script>';
+    res.locals.usuarios = [
+        { nombre: 'Smith', edad: 30 },
+        { nombre: 'Brown', edad: 37 },
+        { nombre: 'Anderson', edad: 29 }
+    ]
+    res.locals.condicion = {
+        segundo,
+        esPar: segundo % 2 === 0
+    }
+    res.render('index');
 });
 
 router.get('/otherroute', (req, res, next) => {
     res.send('Respuesta...')
-})
+});
+
+
+router.get('/pathparam/:dato(\\d+)', (request, response, next) => {
+    const dato = request.params.dato;
+    console.log(request.params);
+    response.send(`Paramatero en ruta --> ${dato}`);
+});
+
+//parmaétro con expresión regular talla --> (M|L|XL)
+//parámetro opcional color -->  http://localhost:3000/talla/1/color/
+router.get('/talla/:talla(M|L|XL)/color/:color?', (request, response, next) => {
+    const talla = request.params.talla;
+    const color = request.params.color;
+    console.log(request.params);
+    response.send(`Talla: ${talla} | Color: ${color}`);
+});
+
+
+// http://localhost:3000/queryparams?talla=XL&color=rojo
+router.get('/queryparams', [
+        //MDW de validación numérica, antes que el MDW que responda, 
+        //el parámetro es el mensaje de error
+        query('talla').isNumeric().withMessage('DEBE SER NUMERICA'),
+        query('color').custom((value) => { return value === 'red'; }).withMessage('MUST BE red') //validación custom
+    ],
+    (request, response, next) => {
+        validationResult(request).throw(); //verifica las validaciones de esta petición
+        const talla = request.query.talla;
+        const color = request.query.color;
+        // if (color !== 'rojo') {
+        //     next(new Error('Only "rojo" allowed'));
+        //     return;
+        // }
+
+        console.log(request.params);
+        response.send(`QueryParams --> Talla: ${talla} | Color: ${color}`);
+    });
+
+
+router.post('/bodyparams', (request, response, next) => {
+    const nombre = request.body['nombre'];
+    console.log(request.body);
+    response.send(`Recibido el nombre ${nombre}`);
+});
 
 module.exports = router;
